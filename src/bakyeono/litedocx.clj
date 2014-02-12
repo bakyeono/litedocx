31b1b3e2ce6164d3903ee88435cf3af3302a2e0c
Bak Yeon O <bakyeono@gmail.com>
2014-02-13 02:59:26 +0900

    Fix missing function calls

--- src/bakyeono/litedocx.clj
+++ src/bakyeono/litedocx.clj
@@ -1,7 +1,7 @@
 (ns bakyeono.litedocx
   "litedocx is a light-weight DOCX format writer library for Clojure."
   (:use [bakyeono.litedocx.util])
-  (:use [bakyeono.litedocx.wpml]))
+  (:require [bakyeono.litedocx.wpml :as w]))
 
 (defrecord Resource [id type content-type part-name])
 
@@ -10,13 +10,13 @@
 (defn create-docx-package
   "Creates DOCX package."
   [dst]
-  (let [content-types-xml (snippet-content-types-xml [])
-        doc-props-app-xml (snippet-doc-props-app-xml)
-        doc-props-core-xml (snippet-doc-props-core-xml)
-        word-document-xml (snippet-word-document-xml)
-        word-styles-xml (snippet-word-styles-xml [])
+  (let [content-types-xml (w/content-types-xml [])
+        doc-props-app-xml (w/doc-props-app-xml)
+        doc-props-core-xml (w/doc-props-core-xml)
+        word-document-xml (w/word-document-xml)
+        word-styles-xml (w/word-styles-xml [])
         word-media-resources nil
-        word-rels-document-xml-rels (snippet-word-rels-document-xml-rels [])]
+        word-rels-document-xml-rels (w/word-rels-document-xml-rels [])]
     (write-zip dst
                "[Content_Types].xml" content-types-xml
                "docProps/app.xml" doc-props-app-xml

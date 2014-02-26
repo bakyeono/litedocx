ed3e4dda6196d6e9904779fd1160aaff5191833d
Bak Yeon O <bakyeono@gmail.com>
2014-02-27 02:22:02 +0900

    Change name of unit twip -> dxa in litedocx.unit package

--- src/bakyeono/litedocx/unit.clj
+++ src/bakyeono/litedocx/unit.clj
@@ -9,49 +9,49 @@
 (defconst cm-per-inch 2.54)
 (defconst inch-per-emu 1/914400)
 (defconst mm-per-cm 10)
+(defconst pt-per-dxa 1/20)
 (defconst pt-per-px 3/4)
-(defconst pt-per-twip 1/20)
 (defconst px-per-inch 96)
 
 ;;; Reversion
 (defconst inch-per-cm (/ 1 cm-per-inch))
 (defconst emu-per-inch (/ inch-per-emu))
 (defconst cm-per-mm (/ 1 mm-per-cm))
+(defconst dxa-per-pt (/ pt-per-dxa))
 (defconst px-per-pt (/ 1 pt-per-px))
-(defconst twip-per-pt (/ pt-per-twip))
 (defconst inch-per-px (/ 1 px-per-inch))
 
 ;;; Combination
 (defconst cm-per-emu (* cm-per-inch inch-per-emu))
 (defconst cm-per-px (* cm-per-inch inch-per-px))
 (defconst cm-per-pt (* cm-per-px px-per-pt))
-(defconst cm-per-twip (* cm-per-pt pt-per-twip))
+(defconst cm-per-dxa (* cm-per-pt pt-per-dxa))
 (defconst inch-per-mm (* inch-per-cm cm-per-mm))
 (defconst inch-per-pt (* inch-per-px px-per-pt))
-(defconst inch-per-twip (* inch-per-pt pt-per-twip))
+(defconst inch-per-dxa (* inch-per-pt pt-per-dxa))
 (defconst mm-per-inch (* mm-per-cm cm-per-inch))
 (defconst mm-per-emu (* mm-per-inch inch-per-emu))
 (defconst mm-per-px (* mm-per-inch inch-per-px))
 (defconst mm-per-pt (* mm-per-px px-per-pt))
-(defconst mm-per-twip (* mm-per-pt pt-per-twip))
+(defconst mm-per-dxa (* mm-per-pt pt-per-dxa))
 (defconst pt-per-inch (* pt-per-px px-per-inch))
 (defconst pt-per-emu (* pt-per-inch inch-per-emu))
 (defconst pt-per-cm (* pt-per-inch inch-per-cm))
 (defconst pt-per-mm (* pt-per-cm cm-per-mm))
+(defconst dxa-per-px (* dxa-per-pt pt-per-px))
+(defconst dxa-per-inch (* dxa-per-px px-per-inch))
+(defconst dxa-per-emu (* dxa-per-inch inch-per-emu))
+(defconst dxa-per-cm (* dxa-per-inch inch-per-cm))
+(defconst dxa-per-mm (* dxa-per-cm cm-per-mm))
 (defconst px-per-emu (* px-per-inch inch-per-emu))
 (defconst px-per-cm (* px-per-inch inch-per-cm))
 (defconst px-per-mm (* px-per-cm cm-per-mm))
-(defconst px-per-twip (* px-per-pt pt-per-twip))
-(defconst twip-per-px (* twip-per-pt pt-per-px))
-(defconst twip-per-inch (* twip-per-px px-per-inch))
-(defconst twip-per-emu (* twip-per-inch inch-per-emu))
-(defconst twip-per-cm (* twip-per-inch inch-per-cm))
-(defconst twip-per-mm (* twip-per-cm cm-per-mm))
+(defconst px-per-dxa (* px-per-pt pt-per-dxa))
 (defconst emu-per-cm (* emu-per-inch inch-per-cm))
 (defconst emu-per-mm (* emu-per-cm cm-per-mm))
 (defconst emu-per-px (* emu-per-inch inch-per-px))
 (defconst emu-per-pt (* emu-per-px px-per-pt))
-(defconst emu-per-twip (* emu-per-pt pt-per-twip))
+(defconst emu-per-dxa (* emu-per-pt pt-per-dxa))
 
 ;;; Functions
 (defn parse-unit
@@ -82,10 +82,10 @@
 
   Examples:
   - (convert \"10px\" \"mm\")
-  - (convert \"29.7 cm\" :twip)
+  - (convert \"29.7 cm\" :dxa)
 
   Supported Unit Types:
-  - Length Units: inch, cm, mm, px, pt, twip"
+  - Length Units: inch, cm, mm, px, pt, dxa, emu"
   [value to]
   (let [[n unit] (parse-unit value)
         rate (conversion-rate unit (name to))]
